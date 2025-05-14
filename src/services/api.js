de76edbff389034b9ab7692e83247cbdf7289988
dbpthk <58109761+dbpthk@users.noreply.github.com>
2025-05-14 23:55:04 +1000

    source files added

---
+++ src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "1a186af7a7b62581c6118e3efd8624c5";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
